0af5e2e118bb586294f5b0762f8939dd134724b8
Your Name <you@example.com>
2025-11-19 10:45:20 +0330

    refactor: update layout and fix styles in list.blade.php

--- resources/views/admin/admin/list.blade.php
+++ resources/views/admin/admin/list.blade.php
@@ -1,30 +1,34 @@
 @extends('admin.layouts.app')
-@section('style')
-    <style>
 
-    </style>
-@endsection
 @section('content')
 <div class="content-section" dir="rtl">
-    <div class="card mb-4">
+     <div class="seven mt-3">
+        <h1>لیست ادمین ها</h1>
+    </div>
+    <div class="card mb-4 p-3">
         <div class="card-header row justify-between-end">
-            <h3 class="card-title col-sm-6">لیست ادمین ها:</h3>
-            <div class="col-sm-6" style="text-align: end;">
-                <a href="{{ url('admin/admin/add') }}" class="btn btn-primary" >اضافه کردن ادمین</a>
+            
+            <div class="col-12 px-0">
+                <a href="{{ url('admin/admin/add') }}" class="btn btn-primary">
+                    <svg width="20" height="20" fill="currentColor" class="bi bi-person-fill-add" viewBox="0 0 16 16">
+                        <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
+                    </svg>
+                    اضافه کردن ادمین</a>
             </div>
 
         </div>
         @include('admin.layouts._message')
         <!-- /.card-header -->
-        <div class="card-body p-0">
-            <table class="table table-striped" role="table">
-                <thead>
+        <div class="table-responsive border shadow-sm rounded bg-white" style="max-height:66vh; overflow-y: auto;" dir="ltr">
+            <table class="table table-hover table-striped align-middle mb-0 text-center" dir="rtl" role="table">
+              <thead class="table-blue">
                 <tr>
-                    <th >#</th>
-                    <th >نام</th>
-                    <th >ایمیل</th>
-                    <th >وضعیت</th>
-                    <th >عملیات</th>
+                    <th style="width: 3%;">#</th>
+                    <th style="width: 20%;">نام</th>
+                    <th style="width: 40%;">ایمیل</th>
+                    <th style="width: 10%;">وضعیت</th>
+                    <th style="width: 10%;">عملیات</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -35,8 +39,8 @@
                         <td>{{ $value->name }}</td>
                         <td>{{ $value->email }}</td>
                         <td>{{ ($value->status == 0) ? 'Active' : 'Inactive' }}</td>
-                        <td>
-                            <a href="{{ url('admin/admin/edit/'.$value->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                        <td class="d-flex flex-row">
+                            <a href="{{ url('admin/admin/edit/'.$value->id) }}" class="btn btn-sm btn-primary mx-1">Edit</a>
                             <a href="{{ url('admin/admin/delete/'.$value->id) }}" class="btn btn-sm btn-danger">Delete</a>
                         </td>
                     </tr>
