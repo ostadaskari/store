4abc4c954e62c7de3a28bc3a97f57aa3e1fa35be
Your Name <you@example.com>
2025-12-11 12:43:03 +0330

    edit icon for مانده اعتبار پیامکی

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -62,7 +62,9 @@
                             <div class="card" style="height: 100px;">
                                 <div class="card-body d-flex align-items-center justify-content-around">
                                     <div class="icon-glass icon-orange">
-                                        <i class="bi bi-chat-left-text-fill mt-3" style="font-size:33px !important;"></i>
+                                        <svg width="35" height="35" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
+                                            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
+                                        </svg>
                                     </div>
                                     <div class="pe-3">
                                         <h5 class="card-title text-start">۸۳۴.۰۰۰</h5>
