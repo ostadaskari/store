b6c60dca4f6d654ba74b168a0dd318d329c84910
Your Name <you@example.com>
2025-12-29 16:14:15 +0330

    edit product card

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1489,119 +1489,196 @@
         محصولات</p>
     <div class="row m-auto my-5">
         <div class="col-12 col-md-3 product-card">
-            <div class="pro my-0">
-                <div class="triangle-badge">
-                    <span class="sale">تخفیف</span>
-                </div>
-                <span class="position-absolute translate-middle badge badge-starPro d-flex flex-row align-items-center">
-                <p class="">3.4</p>
-                <svg width="16" height="16" fill="#161313" class="bi bi-star-fill" viewBox="0 0 16 16">
-                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                </svg>
-              </span>
-                <div class="top">
-                    <img src="design/image/PM-AD40.png" loading="lazy" alt="PM-AD40">
-                </div>
-                <div class="product-name">
-                    ترانسمیتر آنالوگ ورودی 4 کانال PM-AD40
-                </div>
-                <div class="down">
-                    <div class="box">
-                        <div class="orginal">985,000 تومان</div>
-                        <div class="off">40% -</div>
-                    </div>
-                    <div class="final-price-div">
-                        <div class="mx-1">540,000</div>
-                        <div>تومان</div>
+            <div class="pro position-relative"> 
+                <div class="product-overlay">
+                    <button class="btnSvg">
+                        <svg width="20" height="20" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"></path>
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"></path>
+                        </svg>
+                    </button>
+                    <div class="my-2">
+                       <a href="" class="wishlist-btn">
+                          <i class="bi bi-heart" style="font-size:20px"></i>
+                       </a>
                     </div>
                 </div>
+        
+                <a href="" class="text-decoration-none">
+
+                    <div class="top">
+                        <img src="design/image/PM-AD40.png" loading="lazy" alt="PM-AD40">
+                    </div>
+                    <div class="product-name">
+                        <span class="text-dark">P/N : 293D476X0035E2TE3</span>
+                    </div>
+                    <div class="down">
+                        <p class="card-text text-muted mb-1" dir="ltr">
+                            Tantalum Capacitors - Solid SMD 47uF 35volts 20% E Case Mold...
+                        </p>
+
+                        <div class="final-price-div mb-2">
+                        <span class="fw-bold text-dark">104,080 تومان</span>
+                        </div>
+
+                        <div class="box">
+                            <div class="text-danger Quantity-stock">
+                                موجودی: 22 عدد
+                            </div>
+                            <button class="btnSvg">
+                                <svg width="24" height="24" fill="green" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                                    <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"></path>
+                                    <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
         <div class="col-12 col-md-3 product-card">
-            <div class="pro my-0">
-                <div class="triangle-badge">
-                    <span class="sale">تخفیف</span>
-                </div>
-                <span class="position-absolute translate-middle badge badge-starPro d-flex flex-row align-items-center">
-                <p class="">3.4</p>
-                <svg width="16" height="16" fill="#161313" class="bi bi-star-fill" viewBox="0 0 16 16">
-                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                </svg>
-              </span>
-                <div class="top">
-                    <img src="design/image/PM-AD40.png" loading="lazy" alt="PM-AD40">
-                </div>
-                <div class="product-name">
-                    ترانسمیتر آنالوگ ورودی 4 کانال PM-AD40
-                </div>
-                <div class="down">
-                    <div class="box">
-                        <div class="orginal">985,000 تومان</div>
-                        <div class="off">40% -</div>
-                    </div>
-                    <div class="final-price-div">
-                        <div class="mx-1">540,000</div>
-                        <div>تومان</div>
+            <div class="pro position-relative"> 
+                <div class="product-overlay">
+                    <button class="btnSvg">
+                        <svg width="20" height="20" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"></path>
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"></path>
+                        </svg>
+                    </button>
+                    <div class="my-2">
+                        <a href="" class="wishlist-btn">
+                           <i class="bi bi-heart" style="font-size:20px"></i>
+                        </a>
                     </div>
                 </div>
+        
+                <a href="" class="text-decoration-none">
+
+                    <div class="top">
+                        <img src="design/image/PM-AD40.png" loading="lazy" alt="PM-AD40">
+                    </div>
+                    <div class="product-name">
+                        <span class="text-dark">P/N : 293D476X0035E2TE3</span>
+                    </div>
+                    <div class="down">
+                        <p class="card-text text-muted mb-1" dir="ltr">
+                            Tantalum Capacitors - Solid SMD 47uF 35volts 20% E Case Mold...
+                        </p>
+
+                        <div class="final-price-div mb-2">
+                        <span class="fw-bold text-dark">104,080 تومان</span>
+                        </div>
+
+                        <div class="box">
+                            <div class="text-danger Quantity-stock">
+                                موجودی: 22 عدد
+                            </div>
+                            <button class="btnSvg">
+                                <svg width="24" height="24" fill="green" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                                    <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"></path>
+                                    <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
         <div class="col-12 col-md-3 product-card">
-            <div class="pro my-0">
-                <div class="triangle-badge">
-                    <span class="sale">تخفیف</span>
-                </div>
-                <span class="position-absolute translate-middle badge badge-starPro d-flex flex-row align-items-center">
-                <p class="">3.4</p>
-                <svg width="16" height="16" fill="#161313" class="bi bi-star-fill" viewBox="0 0 16 16">
-                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                </svg>
-              </span>
-                <div class="top">
-                    <img src="design/image/PM-AD40.png" loading="lazy" alt="PM-AD40">
-                </div>
-                <div class="product-name">
-                    ترانسمیتر آنالوگ ورودی 4 کانال PM-AD40
-                </div>
-                <div class="down">
-                    <div class="box">
-                        <div class="orginal">985,000 تومان</div>
-                        <div class="off">40% -</div>
-                    </div>
-                    <div class="final-price-div">
-                        <div class="mx-1">540,000</div>
-                        <div>تومان</div>
+            <div class="pro position-relative"> 
+                <div class="product-overlay">
+                    <button class="btnSvg">
+                        <svg width="20" height="20" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"></path>
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"></path>
+                        </svg>
+                    </button>
+                    <div class="my-2">
+                       <a href="" class="wishlist-btn">
+                        <i class="bi bi-heart" style="font-size:20px"></i>
+                       </a>
                     </div>
                 </div>
+        
+                <a href="" class="text-decoration-none">
+
+                    <div class="top">
+                        <img src="design/image/PM-AD40.png" loading="lazy" alt="PM-AD40">
+                    </div>
+                    <div class="product-name">
+                        <span class="text-dark">P/N : 293D476X0035E2TE3</span>
+                    </div>
+                    <div class="down">
+                        <p class="card-text text-muted mb-1" dir="ltr">
+                            Tantalum Capacitors - Solid SMD 47uF 35volts 20% E Case Mold...
+                        </p>
+
+                        <div class="final-price-div mb-2">
+                        <span class="fw-bold text-dark">104,080 تومان</span>
+                        </div>
+
+                        <div class="box">
+                            <div class="text-danger Quantity-stock">
+                                موجودی: 22 عدد
+                            </div>
+                            <button class="btnSvg">
+                                <svg width="24" height="24" fill="green" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                                    <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"></path>
+                                    <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
+
         <div class="col-12 col-md-3 product-card">
-            <div class="pro my-0">
-                <div class="triangle-badge">
-                    <span class="sale">تخفیف</span>
-                </div>
-                <span class="position-absolute translate-middle badge badge-starPro d-flex flex-row align-items-center">
-                <p class="">3.4</p>
-                <svg width="16" height="16" fill="#161313" class="bi bi-star-fill" viewBox="0 0 16 16">
-                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                </svg>
-              </span>
-                <div class="top">
-                    <img src="design/image/PM-AD40.png" loading="lazy" alt="PM-AD40">
-                </div>
-                <div class="product-name">
-                    ترانسمیتر آنالوگ ورودی 4 کانال PM-AD40
-                </div>
-                <div class="down">
-                    <div class="box">
-                        <div class="orginal">985,000 تومان</div>
-                        <div class="off">40% -</div>
-                    </div>
-                    <div class="final-price-div">
-                        <div class="mx-1">540,000</div>
-                        <div>تومان</div>
+            <div class="pro position-relative"> 
+                <div class="product-overlay">
+                    <button class="btnSvg">
+                        <svg width="20" height="20" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"></path>
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"></path>
+                        </svg>
+                    </button>
+                    <div class="my-2">
+                       <a href="" class="wishlist-btn">
+                         <i class="bi bi-heart" style="font-size:20px"></i>
+                       </a>
                     </div>
                 </div>
+        
+                <a href="" class="text-decoration-none">
+
+                    <div class="top">
+                        <img src="design/image/PM-AD40.png" loading="lazy" alt="PM-AD40">
+                    </div>
+                    <div class="product-name">
+                        <span class="text-dark">P/N : 293D476X0035E2TE3</span>
+                    </div>
+                    <div class="down">
+                        <p class="card-text text-muted mb-1" dir="ltr">
+                            Tantalum Capacitors - Solid SMD 47uF 35volts 20% E Case Mold...
+                        </p>
+
+                        <div class="final-price-div mb-2">
+                        <span class="fw-bold text-dark">104,080 تومان</span>
+                        </div>
+
+                        <div class="box">
+                            <div class="text-danger Quantity-stock">
+                                موجودی: 22 عدد
+                            </div>
+                            <button class="btnSvg">
+                                <svg width="24" height="24" fill="green" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                                    <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"></path>
+                                    <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
--- resources/views/front/products/single.blade.php
+++ resources/views/front/products/single.blade.php
@@ -318,9 +318,25 @@
         @if($relatedProducts->count() > 0)
             <div class="row mt-2">
 
+
+
                 @foreach($relatedProducts as $item)
                     <div class="col-12 col-md-3 p-0 px-2 product-card">
                         <div class="pro position-relative"> {{-- Added position-relative --}}
+
+                            <div class="product-overlay">
+                                <button class="btnSvg">
+                                    <svg width="20" height="20" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"></path>
+                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"></path>
+                                    </svg>
+                                </button>
+                                <div class="my-2">
+                                <a href="" class="wishlist-btn">
+                                    <i class="bi bi-heart" style="font-size:20px"></i>
+                                </a>
+                                </div>
+                            </div>
 
                             {{-- Dynamic Discount Badge for Related Products --}}
                             @if($item->price && $item->price->discount_percent > 0)
@@ -330,13 +346,13 @@
                                 </div>
                             @endif
 
-                            <div class="top mt-2">
+                            <div class="top">
                                 {{-- Fixed variable name from $product to $item for the image --}}
-                                <img src="{{$item->coverImage->url ?? asset('images/300x300.webp') }}" alt="{{ $item->company_cmt }}">
+                                <img src="{{$item->coverImage->url ?? asset('images/300x300.webp') }}"  loading="lazy" alt="{{ $item->company_cmt }}">
                             </div>
 
                             <div class="product-name">
-                                <span>{{ $item->company_cmt}}</span>
+                                <span class="text-dark">{{ $item->company_cmt}}</span>
                             </div>
 
                             <div class="down">
@@ -364,7 +380,7 @@
 
                                 <div class="box">
                                     @if($item->available_qty > 0)
-                                        <div class="text-success">
+                                        <div class="text-danger Quantity-stock">
                                             <span class="Quantity-stock">موجود در انبار</span>
                                         </div>
                                     @else
@@ -373,16 +389,16 @@
                                         </div>
                                     @endif
 
-                                    <button class="addtocart">
-                                        خرید
+                                    <button class="btnSvg">
+                                        <svg width="24" height="24" fill="green" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                                            <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"></path>
+                                            <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"></path>
+                                        </svg>
                                     </button>
                                 </div>
 
                             </div>
-                            <!-- ⚠ Overlay  -->
-                            <div class="product-overlay" style="height: 65%;">
-                                <a href="{{ route('front.product.show', ['category' => $item->category->slug, 'product' => $item->slug]) }}" class="btn btn-danger">جزئیات بیشتر</a>
-                            </div>
+                            
 
                         </div>
                     </div>
