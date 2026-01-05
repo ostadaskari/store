c758302511c27b8ba238abc0aec38a0b0fa52af4
Your Name <you@example.com>
2026-01-05 09:22:47 +0330

    edit style user acount

--- resources/views/user/layouts/sidebar.blade.php
+++ resources/views/user/layouts/sidebar.blade.php
@@ -1,9 +1,14 @@
 <!-- sidebar-->
-<div class="col-md-3 mb-4" style="overflow: visible;position: relative;">
+<div class="col-md-3" style="overflow: visible;position: relative;">
     <div class="user-profile">
         <div class="avatar-div d-flex align-items-center flex-column">
+            
             <picture>
-                <img id="avatarPreview" class="avatar-img img-fluid" src="{{ asset('design/image/avatar.jpg') }}" alt="آواتار">
+                <svg  width="80" height="80" fill="#97a6bb" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                </svg>
+                {{-- <img id="avatarPreview" class="avatar-img img-fluid" src="{{ asset('design/image/avatar.jpg') }}" alt="آواتار"> --}}
             </picture>
         </div>
 
