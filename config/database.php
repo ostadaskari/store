92c7abcf5183103e908f6b8750698b5c31801628
ostadaskari <askaraskarinejad@outlook.com>
2026-08-19 23:20:39 -0700

    git ignore updated to for two last line of gitignore

--- config/database.php
+++ config/database.php
@@ -70,7 +70,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => null, //in host of python i set it as InnoDB
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
